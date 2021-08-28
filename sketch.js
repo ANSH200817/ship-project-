8a3bb170579162788737a105b403f7f65da826f8
ANSH200817 <89297724+ANSH200817@users.noreply.github.com>
2021-08-28 10:31:04 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,18 +2,21 @@ var ship
 var groundImage 
 
 function preload(){
-ship  = loadAnimation('ship-1.png')
-groundImage = loadAnimation('sea.png')
+shipimage  = loadAnimation('ship-1.png','ship-2.png','ship-3.png')
+groundImageimage = loadAnimation('sea.png')
 }
 
 function setup(){
   createCanvas(400,400);
-  ship = createSprite(50,160,20,50)
-ship.scale=0.5
+  ship = createSprite(300,160,20,50)
+  ship.addAnimation('nitro',shipimage)
+ship.scale=0.2
 ship.x=50
+groundImage = createSprite(200,180,20,20)
+groundImage.addAnimation('plasma',groundImageimage)
 }
 
 function draw() {
   background("blue");
- 
+ drawSprites()
 }
